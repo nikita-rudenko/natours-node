413ae3348259d0769a50724357c53eca1efdc3df
Nick Rudenko <rdnkta@gmail.com>
2019-09-08 21:06:39 +0300

    responding to URL params

--- app.js
+++ app.js
@@ -9,12 +9,21 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find(el => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
   res.status(200).json({
     status: 'success',
-    results: tours.length,
     data: {
-      tours
+      tour
     }
   });
 });
