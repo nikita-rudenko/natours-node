68b5e966baf6fff042d1e0e294eb9709130f87e1
Nick Rudenko <rdnkta@gmail.com>
2019-09-08 15:46:19 +0300

    handling GET request

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
+
+const port = 3000;
+app.listen(port);
