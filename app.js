dc768e17f9b63dc2b5d6a542be3d0a13ac6ec4ae
Nick Rudenko <rdnkta@gmail.com>
2019-09-15 00:13:39 +0300

    Serve static files

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 
+// app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
