2fd9c9814b8aba7dbb88f7f182750a662d9affed
Nick Rudenko <rdnkta@gmail.com>
2019-09-15 00:45:50 +0300

    Create and use environment variables

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-// Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
