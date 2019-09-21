654aeb8c618427ad4af5318e2f9d24428e4f9abe
Nick Rudenko <rdnkta@gmail.com>
2019-09-21 23:44:29 +0300

    Creating Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,4 @@
-// const Tour = require('../models/tourModel');
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Name and price are required'
-    });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -30,13 +20,22 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success'
-    // data: {
-    //   tour: newTour
-    // }
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent'
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
