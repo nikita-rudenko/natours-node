6acd141a75d8bb5d462913108c8f84a5de6ee880
Nick Rudenko <rdnkta@gmail.com>
2019-09-23 22:49:05 +0300

    Making the API Better: Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,6 +30,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = Number(req.query.page) || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page doesn`t exist');
+    }
+
     // const query = Tour.find()
     //   .where('duration')
     //   .equals(5)
