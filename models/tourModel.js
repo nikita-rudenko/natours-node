81a6dcc815d2fd927edba5faa431d88396c86848
Nick Rudenko <rdnkta@gmail.com>
2019-09-29 19:22:10 +0300

    Aggregation Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -102,6 +102,13 @@ tourSchema.pre(/^find/, function(next) {
 //   next();
 // });
 
+// Aggregation middleware
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
