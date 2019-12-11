e189c08612c3f7a29a9cf4884a1c117e3fe84b57
Nikita Rudenko <rdnkta@gmail.com>
2019-12-11 22:53:14 +0200

    Implementing a Global Error Handling Middleware

--- app.js
+++ app.js
@@ -25,9 +25,25 @@ app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    statud: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`
+  // res.status(404).json({
+  //   statud: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`
+  // });
+
+  const error = new Error("Can't find");
+  error.status = 'fail';
+  error.statusCode = 404;
+
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
